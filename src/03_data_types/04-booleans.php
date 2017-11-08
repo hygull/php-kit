e56e35f3db4cb80acc59d8f91883016bbbeb6737
hygull <rishikesh0014051992@gmail.com>
2017-11-08 23:45:40 +0530

    13 booleans and their usage(true=>'1' & false=>'0')

---
+++ src/03_data_types/04-booleans.php
@@ -0,0 +1,37 @@
+<?php
+/*
+	{
+		"createdOn": "08 Nov 2017, Wed",
+		"aim": "Playing with booleans",
+		"codedBy": "Rishikesh Agrawani"
+	}
+*/
+
+// Initializing floats
+$iAmOld = false;
+$mathIsNice = TRUE;
+
+// echo $mathIsNice, "\n";	// "1"
+// echo $iAmOld;			// ""
+
+# Definition of message() function
+function message($boolValue) {
+	if($boolValue) {
+		echo "TRUE: ";
+	} else {
+		echo "FALSE: ";
+	}
+	echo "GOT value as ", (int)$boolValue, "\n";
+}
+
+// Calling message() function 
+message($iAmOld);
+message($mathIsNice);
+?>
+
+<?php
+/*
+FALSE: GOT value as 0
+TRUE: GOT value as 1
+*/
+?>
